028361647e537a936e225cd1e3b303d9f816b14a
simonhollands <hollands@Simons-MacBook-Pro.local>
2019-10-31 08:08:29 -0700

    new

--- tests/test_in.py
+++ tests/test_in.py
@@ -4,3 +4,5 @@ def test_answer():
     assert 1==0  # to see what was printed
 def test_answer2():
     assert 1==1  # to see what was printed
+def test_answer2():
+    assert 1==2  # to see what was printed
