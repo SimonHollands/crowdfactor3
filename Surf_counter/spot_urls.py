397b9365acad2d4e4abc0bf2719c4ead8d9c0426
simonhollands <hollands@Simons-MacBook-Pro.local>
2019-11-03 16:14:30 -0800

    message

--- Surf_counter/spot_urls.py
+++ Surf_counter/spot_urls.py
@@ -2,6 +2,7 @@
 class SpotUrls:
     token='85392160'
     CFID='459565'
+    venice_morning_good='https://camrewinds.cdn-surfline.com/live/wc-venicebeachclose.stream.20191103T162900647.mp4'
     venice_static='https://camrewinds.cdn-surfline.com/live/wc-venicebeachclose.stream.20191027T235900139.mp4'
     lookup={'venice_beach': 'http://www.surfline.com/surfdata/video-rewind/video_rewind.cfm?id=150603&CFID=459565&CFTOKEN=85392160'}
                              
